fd0b74ac67516fa8ae877136197c0d9e2527c697
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2020-10-29 13:51:48 -0400

    connected users table to userinfo when you create the account. We set the userinfo data to automatically be 0, so the user can edit the info within the main window. That info is shown on main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -27,10 +27,20 @@ void MainWindow::loadData(){
     name.prepare("select firstname, lastname from users where ID='" + getUser() + "';");
     name.exec();
     name.next();
+    QSqlQuery userInfo;
+    userInfo.prepare("select * from userinfo where UserID='" + getUser() + "';");
+    userInfo.exec();
+    userInfo.next();
     ui->firstname->setText(name.value(0).toString());
     ui->lastname->setText(name.value(1).toString());
     ui->UserFName->setText(name.value(0).toString());
     ui->UserLName->setText(name.value(1).toString());
+    ui->UserAge->setText(userInfo.value(1).toString());
+    ui->UserWeight->setText(userInfo.value(2).toString());
+    ui->UserHeight->setText(userInfo.value(3).toString());
+    ui->UserGender->setText(userInfo.value(4).toString());
+    ui->UserGoal->setText(userInfo.value(5).toString());
+
 }
 
 void MainWindow::setUser(QString rec){
