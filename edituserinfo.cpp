ef84853fa8a30c53f262636fae78baab8ed59a7f
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2020-11-04 23:24:30 -0500

    added field validations and integer validations for the edit user page. Also changed age and height on the UI. Added a close button as well.

--- edituserinfo.cpp
+++ edituserinfo.cpp
@@ -39,12 +39,15 @@ void EditUserInfo::on_editButton_clicked()
         goalID = "3";
     }
 
-    updatedata.prepare(
-            "UPDATE userinfo SET Age='" + age + "', Weight='" + weight + "', HeightInches='" + height + "', Gender='" + gender + "', Goal='" + goalID + "' WHERE userID='" + getUser() + "'"
-            );
-    updatedata.exec();
-    //qDebug() << updatedata.lastQuery();
-    close();
+    if (fieldValidation()) {
+        updatedata.prepare(
+                "UPDATE userinfo SET Age='" + age + "', Weight='" + weight + "', HeightInches='" + height +
+                "', Gender='" + gender + "', Goal='" + goalID + "' WHERE userID='" + getUser() + "'"
+        );
+        updatedata.exec();
+        //qDebug() << updatedata.lastQuery();
+        close();
+    }
 }
 
 void EditUserInfo::setUser(QString rec) {
@@ -53,4 +56,56 @@ void EditUserInfo::setUser(QString rec) {
 
 QString EditUserInfo::getUser() {
     return received;
+}
+
+bool EditUserInfo::fieldValidation() {
+    const QString age = ui->userAge->text();
+    const QString height = ui->userHeight->text();
+    const QString weight = ui->userWeight->text();
+    const QString gender = ui->userGender->text();
+    const QString goal = ui->goalCombo->currentText();
+
+    std::string ageStr = age.toStdString();
+    std::string heightStr = height.toStdString();
+    std::string weightStr = weight.toStdString();
+    std::string genderStr = gender.toStdString();
+    std::string goalStr = goal.toStdString();
+
+    if (ageStr != "" && heightStr != "" && weightStr != "" && genderStr != "" && goalStr != "Make a selection...") {
+        if (checkDigits(ageStr)) {
+            if (checkDigits(heightStr)) {
+                if (checkDigits(weightStr)) {
+                    return true;
+                }
+                else {
+                    QMessageBox msgBox;
+                    msgBox.setText("Weight must be in digit form");
+                    msgBox.exec();
+                }
+            }
+            else {
+                QMessageBox msgBox;
+                msgBox.setText("Height must be in digit form");
+                msgBox.exec();
+            }
+        }
+        else {
+            QMessageBox msgBox;
+            msgBox.setText("Age must be in digit form");
+            msgBox.exec();
+        }
+    }
+    else {
+        QMessageBox msgBox;
+        msgBox.setText("Not all field are filled in!");
+        msgBox.exec();
+    }
+}
+
+bool EditUserInfo::checkDigits(const std::string &str) {
+    return std::all_of(str.begin(), str.end(), ::isdigit);
+}
+void EditUserInfo::on_close_clicked()
+{
+    close();
 }
